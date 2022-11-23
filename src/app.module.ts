3a9cf89829b5c0e04362b6ad9b3d1d5811adf24e
muhgifariakbar <muhgifariakbar@gmail.com>
2022-11-23 22:31:10 +0700

    fixing mongo address

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,9 @@ import { AppService } from './app.service';
 import { EmployeeModule } from './employee/employee.module';
 import { MongooseModule } from '@nestjs/mongoose'; //1
 
+let mongo_address = process.env.mongo_address;
 @Module({
-  imports: [
-    EmployeeModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://admin:12345@cluster0.iwx76dq.mongodb.net/Employee?retryWrites=true&w=majority',
-    ),
-  ],
+  imports: [EmployeeModule, MongooseModule.forRoot(mongo_address)],
   controllers: [AppController],
   providers: [AppService],
 })
